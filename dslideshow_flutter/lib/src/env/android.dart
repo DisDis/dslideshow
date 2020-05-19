dc5997f685c1cf0b998d01aab1afc273c0d21731
Igor Demyanov <igor.demyanov@gmail.com>
2020-05-19 23:37:30 +0300

    migrate to permission_handler

--- dslideshow_flutter/lib/src/env/android.dart
+++ dslideshow_flutter/lib/src/env/android.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:simple_permissions/simple_permissions.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:path_provider/path_provider.dart' as path_prov;
 export 'package:dslideshow_backend/environment.dart';
 
@@ -9,10 +9,16 @@ Future<Directory> getApplicationDocumentsDirectory() {
 }
 
 Future<dynamic> checkPermissionReadExternalStorage() async {
-  var isOk =
-      await SimplePermissions.checkPermission(Permission.ReadExternalStorage);
-  if (!isOk) {
-    return SimplePermissions.requestPermission(Permission.ReadExternalStorage);
+  var status = await Permission.storage.status;
+  if (status.isUndetermined) {
+    // We didn't ask for permission yet.
+    status = await Permission.storage.request();
   }
-  return new Future<dynamic>.value(PermissionStatus.authorized);
+  return status == PermissionStatus.granted;
+//  var isOk =
+//      await SimplePermissions.checkPermission(Permission.ReadExternalStorage);
+//  if (!isOk) {
+//    return SimplePermissions.requestPermission(Permission.ReadExternalStorage);
+//  }
+//  return new Future<dynamic>.value(PermissionStatus.authorized);
 }
