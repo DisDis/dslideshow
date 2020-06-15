22dd9c8d1af62cfd0d43ab170bf58fc0788adc46
Andrey Smirnov <andrey.smirnov@team.wrike.com>
2020-06-15 03:35:16 +0300

    system info metrics widget

--- system_metrics_widget/lib/src/widgets/metrics/network_usage_widget.dart
+++ system_metrics_widget/lib/src/widgets/metrics/network_usage_widget.dart
@@ -4,14 +4,34 @@ import 'package:system_metrics_widget/src/model/system_info/network_info.dart';
 import 'package:system_metrics_widget/src/widgets/metrics/details/metrics_details_widget.dart';
 
 class NetworkUsageWidget extends StatelessWidget {
-  NetworkUsageWidget({@required NetworkInfo model, Key key}) : super(key: key);
+  final NetworkInfo _model;
+  NetworkUsageWidget({@required NetworkInfo model, Key key})
+      : _model = model,
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.green,
-      child: Center(
-        child: MetricsDetails('network'),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(children: [MetricsDetails('network: ${_model.lastUpdate}')]),
+          if (_model.hasInternet)
+            Icon(
+              Icons.cast_connected,
+              color: Colors.white,
+              size: 24.0,
+            )
+          else
+            Icon(
+              Icons.cast,
+              color: Colors.white,
+              size: 24.0,
+            ),
+          for (final interface in _model.interfaces)
+            Row(children: [MetricsDetails('${interface.name} - ${interface.status}')]),
+        ],
       ),
     );
   }
