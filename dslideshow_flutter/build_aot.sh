c0f86aeb0c279285706780c4541740781f5fea47
Igor Demyanov <igor.demyanov@gmail.com>
2020-08-21 13:36:33 +0300

    added AOT

---
+++ dslideshow_flutter/build_aot.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+FLUTTER_PATH=~/bin/flutter
+OUTPUT_KERNEL_DILL=./build/kernel_snapshot.dill
+OUTPUT_KERNEL_D=./build/kernel_snapshot.d
+OUTPUT_SO=./build/app.so
+APPLICATION_ENTRY_POINT=package:dslideshow_flutter/main.dart
+ENGINE_BINARIES=.
+echo "----------- README -----------"
+echo "Instruction: https://github.com/ardera/flutter-pi/tree/engine-binaries"
+echo "git clone --depth 1 --branch engine-binaries https://github.com/ardera/flutter-pi.git flutter_engine_binaries"
+
+echo "------------------------"
+
+CMD="$FLUTTER_PATH/bin/cache/dart-sdk/bin/dart \
+  $FLUTTER_PATH/bin/cache/dart-sdk/bin/snapshots/frontend_server.dart.snapshot \
+  --sdk-root $FLUTTER_PATH/bin/cache/artifacts/engine/common/flutter_patched_sdk_product \
+  --target=flutter \
+  --aot \
+  --tfa \
+  -Ddart.vm.product=true \
+  --packages .packages \
+  --output-dill $OUTPUT_KERNEL_DILL \
+  --verbose \
+  --depfile $OUTPUT_KERNEL_D \
+  $APPLICATION_ENTRY_POINT"
+
+echo "------------------------"
+echo "$CMD"
+eval $CMD
+
+CMD="$ENGINE_BINARIES/gen_snapshot_linux_x64 \
+  --causal_async_stacks \
+  --deterministic \
+  --snapshot_kind=app-aot-elf \
+  --elf=$OUTPUT_SO \
+  --strip \
+  --sim_use_hardfp \
+  --no-use-integer-division \
+  $OUTPUT_KERNEL_DILL"
+
+echo "$CMD"
+eval $CMD
