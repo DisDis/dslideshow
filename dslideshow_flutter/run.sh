b5efec4f3d4b34260a846eb716b734a1a6738bd2
Igor Demyanov <igor.demyanov@gmail.com>
2020-08-24 19:03:02 +0300

    update linux support

---
+++ dslideshow_flutter/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#./screenOn.sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./
+
+#debug mode
+#./flutter-pi ./flutter_assets/
+
+#release mode
+#./flutter-pi -d "192, 120"  --release ./flutter_assets/
+./flutter-pi  --release --no-text-input ./flutter_assets/
