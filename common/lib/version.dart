0175f2a90cc9296641d8c24c2a1d0b4f8e0b4684
Igor Demyanov <igor.demyanov@gmail.com>
2025-05-04 13:03:34 +0300

    remove images, doc to other repo

--- common/lib/version.dart
+++ common/lib/version.dart
@@ -1,4 +1,4 @@
 abstract class ApplicationInfo{
-  static const String frontendVersion = '7.1.0+38';
-  static const String backendVersion = '7.2.0+7';
+  static const String frontendVersion = '7.1.0+39';
+  static const String backendVersion = '7.2.0+8';
 }
