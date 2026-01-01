2a67cdf98f3c12a01b9a7c22f6c232b68e43a50b
Igor Demyanov <igor.demyanov@tripster.ru>
2026-01-02 00:55:26 +0300

    fix offline mode

--- config_app/build_web.sh
+++ config_app/build_web.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 flutter pub get
 #--pwa-strategy none
-flutter build web --release --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/
+flutter build web --release --no-web-resources-cdn
+#--dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/
+#flutter build web --release --web-renderer html
