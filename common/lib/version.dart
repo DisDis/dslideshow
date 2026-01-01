5e71ff2bb045e9cafe91318d73bdb8600a70d16f
Igor Demyanov <igor.demyanov@gmail.com>
2026-01-02 02:54:33 +0300

    bump version

--- common/lib/version.dart
+++ common/lib/version.dart
@@ -1,4 +1,4 @@
 abstract class ApplicationInfo{
-  static const String frontendVersion = '7.5.0+3';
-  static const String backendVersion = '7.5.0+3';
+  static const String frontendVersion = '7.5.0+4';
+  static const String backendVersion = '7.5.0+4';
 }
