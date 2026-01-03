8eed92f43eb1511d001e74f9573c3beaa1c1603a
Igor Demyanov <igor.demyanov@gmail.com>
2026-01-03 20:19:52 +0300

    update file_picker: ^10.3.8

--- common/lib/version.dart
+++ common/lib/version.dart
@@ -1,4 +1,4 @@
 abstract class ApplicationInfo{
-  static const String frontendVersion = '8.2.0+5';
-  static const String backendVersion = '8.2.0+5';
+  static const String frontendVersion = '8.2.0+6';
+  static const String backendVersion = '8.2.0+6';
 }
